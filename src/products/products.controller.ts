fda78ccb3153ae239480a901e066f72c5d32ca14
Francisco Lino <franksuriel_7@hotmail.com>
2024-04-15 22:11:36 -0600

    UPDATE connect product ms clientProxy

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,16 +1,25 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   Delete,
   Get,
+  Inject,
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
+import { PaginationDto } from 'src/common';
+import { PRODUCT_SERVICE } from 'src/config';
 
 @Controller('products')
 export class ProductsController {
-  constructor() {}
+  constructor(
+    @Inject(PRODUCT_SERVICE) private readonly productsClient: ClientProxy,
+  ) {}
 
   @Post()
   createProducts() {
@@ -18,13 +27,23 @@ export class ProductsController {
   }
 
   @Get()
-  findAllProducts() {
-    return 'Return all products';
+  findAllProducts(@Query() paginationDto: PaginationDto) {
+    return this.productsClient.send(
+      { cmd: 'find_all_products' },
+      paginationDto,
+    );
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return `Return product with id ${id}`;
+  async findOne(@Param('id') id: string) {
+    try {
+      const product = await firstValueFrom(
+        this.productsClient.send({ cmd: 'find_one_product' }, { id }),
+      );
+      return product;
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
   }
 
   @Delete(':id')
